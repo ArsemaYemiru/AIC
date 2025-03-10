c6ac2d330e02fe7782064cbb7503387875841c0c
Tnsae <tnsae.alemayehu@bitscollege.edu.et>
2025-03-10 15:35:21 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./Components/nav/header";
+import Footer from "./Components/nav/footer";
+// import Header from "./Components/pages/about";
+// import Header from "./Components/pages/announcements";
+// import Header from "./Components/pages/contacts";
+// import Header from "./Components/pages/home";
+// import Header from "./Components/pages/media";
+// import Header from "./Components/pages/services";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <Router>
       <Routes>
         <Route path="/" element={<>
           <Header />
+          <Footer />
         </>} />
       </Routes>
     </Router>
