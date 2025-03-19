2003ba530df0a81070b3d5f8640e2dd6019595c0
Arsema <arsemayemiru610@gmail.com>
2025-03-19 16:10:30 -0400

    regional branches

--- src/Components/pages/contacts.jsx
+++ src/Components/pages/contacts.jsx
@@ -3,7 +3,7 @@ import Header from "../nav/header";
 import Footer from "../nav/footer";
 import { MapPin, Phone, Building2, Mail } from "lucide-react";
 
-const AA = [
+const addisAbabaBranches = [
     { location: "Head office Bole", phone: "0116518400", email: "underwritingho@africainsurancesc.com" },
     { location: "Addisu Gebeya", phone: "0111264137/0111265764", email: "addisugebiya@africainsurancesc.com" },
     { location: "Arada", phone: "0111119019/0111119022", email: "arada@africainsurancesc.com" },
@@ -28,86 +28,90 @@ const AA = [
     { location: "T/Haimanot", phone: "0112777541/0112777543", email: "teklehaymanot@africainsurancesc.com" },
     { location: "Yoseph", phone: "0114421573/0114401572", email: "yoseph@africainsurancesc.com" },
 ];
-
+const regionalBranches = [
+    { location: "Adama", phone: "0221122101/0221122099", email: "adama@africainsurancesc.com" },
+    { location: "Arba Minch", phone: "0461818636/0461818637", email: "arbaminch@africainsurancesc.com" },
+    { location: "Bahir Dar", phone: "0582262525/0582201792", email: "bahirdar@africainsurancesc.com" },
+    { location: "Debre Berhan", phone: "0116375819/0116376251", email: "debrebrihan@africainsurancesc.com" },
+    { location: "Dessie", phone: "0331119140/0331119141", email: "dessie@africainsurancesc.com" },
+    { location: "Dire Dawa", phone: "0251110715/0251118930", email: "diredawa@africainsurancesc.com" },
+    { location: "Gondar", phone: "0581110234/0581119829", email: "gondar@africainsurancesc.com" },
+    { location: "Hawassa", phone: "0462204765/0462201960", email: "hawassa@africainsurancesc.com" },
+    { location: "Jijiga", phone: "0252783146/0252783147", email: "jijiga@africainsurancesc.com" },
+    { location: "Jimma", phone: "0472110426/0472110396", email: "jimma@africainsurancesc.com" },
+    { location: "Mekele", phone: "03440409211/0344408410", email: "mekelle@africainsurancesc.com" },
+    { location: "Nekemete", phone: "0576600013", email: "nekemte@africainsurancesc.com" },
+    { location: "Sebeta", phone: "0113818022/0113818199", email: "sebeta@africainsurancesc.com" },
+    { location: "Shire", phone: "0342441867", email: "shire@africainsurancesc.com" },
+    { location: "Wolayita Sodo", phone: "0461800983", email: "wolayita@africainsurancesc.com" },
+];
 function Contacts() {
-    const [showMore, setShowMore] = useState(false);
+    const [showMoreAddis, setShowMoreAddis] = useState(false);
+    const [showMoreRegional, setShowMoreRegional] = useState(false);
 
-    const visibleItems = showMore ? AA : AA.slice(0, 8); // Show 6 items by default, all if 'showMore' is true
+    const visibleAddisBranches = showMoreAddis ? addisAbabaBranches : addisAbabaBranches.slice(0, 6);
+    const visibleRegionalBranches = showMoreRegional ? regionalBranches : regionalBranches.slice(0, 6);
 
     return (
         <>
             <Header />
-            <div className="flex flex-row justify-space-between ml-36 mt-10">
-                <div className="w-full max-w-3xl h-[400px] sm:h-[500px] lg:h-[600px] rounded-lg overflow-hidden">
-                    <iframe
-                        title="Location Map"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.7799458220306!2d38.77787627515661!3d8.992385791067488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b858aa4e36b6d%3A0xba3a09392dcba790!2sAFRICA%20INSURANCE%20COMPANY%20S.C.%20HEAD%20OFFICE!5e0!3m2!1sen!2set!4v1741960143704!5m2!1sen!2set"
-                        width="80%"
-                        height="100%"
-                        style={{ border: 0 }}
-                        allowFullScreen
-                        loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"
-                    ></iframe>
-                </div>
-                <div className="text-[#343989] flex flex-col justify-center gap-5">
-                    <div className="flex items-center">
-                        <MapPin className="w-4 h-4" />
-                        <p className="text-sm ml-4">Head Office @ Africa Avenue (Bole Road)</p>
-                    </div>
-                    <div className="flex items-center">
-                        <Phone className="w-4 h-4" />
-                        <p className="text-sm ml-4">251 0116637716 / 17 / 18 / 19</p>
-                    </div>
-                    <div className="flex items-center">
-                        <Building2 className="w-4 h-4" />
-                        <p className="text-sm ml-4">12941, Addis Ababa, Ethiopia</p>
-                    </div>
-                    <div className="flex items-center">
-                        <Mail className="w-4 h-4" />
-                        <p className="text-sm ml-4">md@africainsurancesc.com</p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="flex items-center justify-center mt-6 mb-6">
-                <div className="w-1/3 border-t-2 border-[#343989] mx-4"></div>
+            <div className="flex flex-col items-center mt-10">
                 <h3 className="text-2xl font-semibold text-[#343989]">Addis Ababa Branches</h3>
-                <div className="w-1/3 border-t-2 border-[#343989] mx-4"></div>
-            </div>
-
-            <div className="mt-5 grid grid-cols-1 mx-8 sm:grid-cols-2 md:grid-cols-4 gap-4">
-                {visibleItems.map((item, index) => (
-                    <div
-                        key={index}
-                        className="border border-[#343989] p-4 w-auto rounded-lg flex flex-col gap-3"
-                    >
-                        <div className="flex text-[#343989] items-center gap-2">
-                            <MapPin className="w-5 h-5 text-[#343989]" />
-                            <p className="text-lg font-bold">{item.location}</p>
+                <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+                    {visibleAddisBranches.map((item, index) => (
+                        <div key={index} className="border p-4 rounded-lg flex flex-col gap-3">
+                            <div className="flex items-center gap-2">
+                                <MapPin className="w-5 h-5" />
+                                <p className="text-lg font-bold">{item.location}</p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <Phone className="w-5 h-5" />
+                                <p>{item.phone}</p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <Mail className="w-5 h-5" />
+                                <p>{item.email}</p>
+                            </div>
                         </div>
-                        <div className="flex items-center gap-2">
-                            <Phone className="w-5 h-5 text-[#343989]" />
-                            <p className="text-sm">{item.phone}</p>
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <Mail className="w-5 h-5 text-[#343989]" />
-                            <p className="text-sm">{item.email}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-
-            {AA.length > 8 && (
-                <div className="flex justify-center mt-4">
-                    <button
-                        className="text-[#343989] hover:text-[#673BB7] transition-all"
-                        onClick={() => setShowMore(!showMore)}
-                    >
-                        {showMore ? 'Show Less' : 'More'}
-                    </button>
+                    ))}
                 </div>
-            )}
+                {addisAbabaBranches.length > 6 && (
+                    <button
+                        className="mt-4 text-[#343989] hover:text-[#673BB7]"
+                        onClick={() => setShowMoreAddis(!showMoreAddis)}
+                    >
+                        {showMoreAddis ? "Show Less" : "More"}
+                    </button>
+                )}
+
+                <h3 className="text-2xl font-semibold text-[#343989] mt-10">Regional Branches</h3>
+                <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+                    {visibleRegionalBranches.map((item, index) => (
+                        <div key={index} className="border p-4 rounded-lg flex flex-col gap-3">
+                            <div className="flex items-center gap-2">
+                                <MapPin className="w-5 h-5" />
+                                <p className="text-lg font-bold">{item.location}</p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <Phone className="w-5 h-5" />
+                                <p>{item.phone}</p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <Mail className="w-5 h-5" />
+                                <p>{item.email}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                {regionalBranches.length > 6 && (
+                    <button
+                        className="mt-4 text-[#343989] hover:text-[#673BB7]"
+                        onClick={() => setShowMoreRegional(!showMoreRegional)}
+                    >
+                        {showMoreRegional ? "Show Less" : "More"}
+                    </button>
+                )}
+            </div>
             <Footer />
         </>
     );
