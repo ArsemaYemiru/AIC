88d9035d66decbdd9f0708ac954d04635d2687eb
Tnsae <tnsae.alemayehu@bitscollege.edu.et>
2025-05-21 08:35:38 +0300

    demo

--- src/Components/pages/services/claims.jsx
+++ src/Components/pages/services/claims.jsx
@@ -100,7 +100,9 @@ const Claims = () => {
         <div className="text-3xl mt-8 mb-8 text-[#343989] font-bold">
           <p>Claims</p>
         </div>
-
+        <div>
+          <button className="text-green-500">Submit Claim</button>
+        </div>
         {/* 3-column grid container */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
           {claims.map((claim) => (
